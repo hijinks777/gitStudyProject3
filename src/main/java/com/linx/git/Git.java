0221c487ec30070685ea8677d6a37f936babb514
linx <93718799+hijinks777@users.noreply.github.com>
2023-02-19 19:27:58 +0800

    Update Git.java

--- src/main/java/com/linx/git/Git.java
+++ src/main/java/com/linx/git/Git.java
@@ -9,5 +9,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("hot-fix");
+        System.out.println("hot-fix");
     }
 }
